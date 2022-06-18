05e74cdfaf6ea9701e9d0958c706be89674d7df9
VTL1618 <vitalyzubenkowork@gmail.com>
2022-06-18 11:36:18 +0300

    Initial Commit

---
+++ L10_Networking/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  L10_Networking
+//
+//  Created by Vitaly Zubenko on 18.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
